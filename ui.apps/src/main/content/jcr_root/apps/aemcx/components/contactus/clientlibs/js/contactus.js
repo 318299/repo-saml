87eb2ea129571a9c04ae9ff4aec6711289c2cc86
Nikita <Nikita.Panchadhar@nttdata.com>
2023-07-10 06:42:17 +0000

    adding contact us component

---
+++ ui.apps/src/main/content/jcr_root/apps/aemcx/components/contactus/clientlibs/js/contactus.js
@@ -0,0 +1,9 @@
+function myFunction() {
+var txt;
+  if (confirm("Do you want to submit your details")) {
+  	alert ("Your details are submitted successfully!")
+  }
+else {
+    alert( "Kindly Resubmit the details")
+  }
+}
