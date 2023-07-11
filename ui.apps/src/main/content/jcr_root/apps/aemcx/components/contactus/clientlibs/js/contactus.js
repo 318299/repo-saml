0d069f4a4b988b7845e79bd5d23fc1f03fc9cf09
Nikita <Nikita.Panchadhar@nttdata.com>
2023-07-11 10:17:29 +0000

    checking contact us

--- ui.apps/src/main/content/jcr_root/apps/aemcx/components/contactus/clientlibs/js/contactus.js
+++ ui.apps/src/main/content/jcr_root/apps/aemcx/components/contactus/clientlibs/js/contactus.js
@@ -1,9 +1,33 @@
 function myFunction() {
-var txt;
-  if (confirm("Do you want to submit your details")) {
-  	alert ("Your details are submitted successfully!")
-  }
-else {
-    alert( "Kindly Resubmit the details")
-  }
+    var fnameText;
+    var lnameText;
+    var phnoText;
+    var emailText;
+    if (confirm("Do you want to submit your details")) {
+        alert ("Your details are submitted successfully!")
+    }
+    else {
+        alert( "Kindly Resubmit the details")
+    }
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let phno = document.getElementById("phno").value;
+    let gmail = document.getElementById("gmail").value;
+  // If x is Not a Number or less than one or greater than 10
+    if (isNaN(fname)) {
+        fnameText = "Please enter your first name";
+    }
+    if (isNaN(lname)) {
+        lnameText = "Please enter your last name";
+    }
+    if (isNaN(phno)) {
+        phnoText = "Please enter a valid contact number";
+    }
+    if (isNaN(gmail)) {
+        emailText = "Please enter a valid email id";
+    }
+    document.getElementById("fname-error").innerHTML = fnameText;
+    document.getElementById("lname-error").innerHTML = lnameText;
+    document.getElementById("phno-error").innerHTML = phnoText;
+    document.getElementById("gmail-error").innerHTML = fnameText;
 }
