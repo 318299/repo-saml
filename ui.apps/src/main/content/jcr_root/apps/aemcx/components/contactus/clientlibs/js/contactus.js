972eea124af644036198e2676d0cb1e4a0e77e39
Nikita <Nikita.Panchadhar@nttdata.com>
2023-07-11 11:18:47 +0000

    contactus form & accordion component

--- ui.apps/src/main/content/jcr_root/apps/aemcx/components/contactus/clientlibs/js/contactus.js
+++ ui.apps/src/main/content/jcr_root/apps/aemcx/components/contactus/clientlibs/js/contactus.js
@@ -1,33 +1,26 @@
 function myFunction() {
-    var fnameText;
-    var lnameText;
-    var phnoText;
-    var emailText;
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let phno = document.getElementById("phno").value;
+    let gmail = document.getElementById("gmail").value;
+	console.log("fname="+fname+" lname="+lname+" phno="+phno+" gmail="+gmail);
+    if (isNaN(fname)) {
+        document.getElementById("fname-error").innerHTML = "Please enter your first name";
+    }
+    if (isNaN(lname)) {
+        document.getElementById("lname-error").innerHTML = "Please enter your last name";
+    }
+    if (isNaN(phno)) {
+        document.getElementById("phno-error").innerHTML = "Please enter a valid contact number";
+    }
+    if (isNaN(gmail)) {
+        document.getElementById("gmail-error").innerHTML = "Please enter a valid email id";
+    }
+	console.log("fname error msg: "+document.getElementById("fname-error").innerHTML);
     if (confirm("Do you want to submit your details")) {
         alert ("Your details are submitted successfully!")
     }
     else {
         alert( "Kindly Resubmit the details")
     }
-    let fname = document.getElementById("fname").value;
-    let lname = document.getElementById("lname").value;
-    let phno = document.getElementById("phno").value;
-    let gmail = document.getElementById("gmail").value;
-  // If x is Not a Number or less than one or greater than 10
-    if (isNaN(fname)) {
-        fnameText = "Please enter your first name";
-    }
-    if (isNaN(lname)) {
-        lnameText = "Please enter your last name";
-    }
-    if (isNaN(phno)) {
-        phnoText = "Please enter a valid contact number";
-    }
-    if (isNaN(gmail)) {
-        emailText = "Please enter a valid email id";
-    }
-    document.getElementById("fname-error").innerHTML = fnameText;
-    document.getElementById("lname-error").innerHTML = lnameText;
-    document.getElementById("phno-error").innerHTML = phnoText;
-    document.getElementById("gmail-error").innerHTML = fnameText;
 }
